1cbff43b9f5b89408974d8713221918b71c45def
LaurentNg <laurentnguyen82@gmail.com>
2022-04-10 00:23:16 -0400

    update plant

--- client/src/app/plant/plant.component.ts
+++ client/src/app/plant/plant.component.ts
@@ -1,6 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component } from '@angular/core';
 import { Plant } from '../../../../common/tables/Plant';
 import { CommunicationService } from '../communication.service';
 
@@ -10,16 +8,14 @@ import { CommunicationService } from '../communication.service';
   styleUrls: ['./plant.component.css']
 })
 export class PlantComponent {
-  @ViewChild('plantPaginator') plantPaginator: MatPaginator;
   plantName: string;
-  plants: MatTableDataSource<Plant>;
+  plants: Plant[];
   
   constructor(private communicationService: CommunicationService) { }
 
   public searchPlant(plantName: string) {
     this.communicationService.getPlant(plantName).subscribe((plants: Plant[]) => {
-      this.plants = new MatTableDataSource<Plant>(plants);
-      this.plants.paginator = this.plantPaginator;
+      this.plants = plants;
     });
   }
 }
